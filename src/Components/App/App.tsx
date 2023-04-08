df294ab5a99209b1b53d4f4db4133b3690ab1ac1
Efrem Lapin <efremlapin@vk.com>
2023-04-08 15:25:40 +0300

    project initialization

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,12 @@
+import './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
